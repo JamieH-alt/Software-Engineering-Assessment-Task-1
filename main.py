2ee00352cd1345b477f8466dceb4ffdab8aa77c6
Jamie Hanson <Jamie.hanson@education.nsw.gov.au>
2025-03-11 15:06:58 +1100

    Further work on GUI
    Gui has had more additions, and have started a little bit of the backend that is required to have the GUI load. This is mainly the watchedmovies system, which is currently using a moviestorage class that will be read from a file and implemented, this will store the title, cover and id of the movie.
    This was all so that the tiling storage could work within the UI.
    This has been tested so far in that the program now has a scrollable frame that needs to become horizontally scrolling (currently vertically), that stores the movies and currently (as no movies have been stored / no storage has been implemented) displays an empty "+" icon image, though the image looks horrible at the moment as it was not designed for the current display resolution (will be edited in photoshop at a later date.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import customtkinter
+import math
+import PIL.Image as Images
+import requests
+import os
 
 # Sets the theme / appearance mode, so it doesnt follow the system (Forced dark mode because yes)
 customtkinter.set_default_color_theme("dark-blue")
@@ -18,7 +22,7 @@ class App(customtkinter.CTk):
         self.customtitle.grid(pady=10,padx=5,row=0,column=3,columnspan=11)
         self.customtitle.tag_config("center", justify="center")
         self.customtitle.insert("0.0", "MovieLister", "center")
-        self.configure(state="disabled")
+        self.customtitle.configure(state="disabled")
 
         # Sets up the search bar and its storage.
         self.searchtext = ""
@@ -33,16 +37,77 @@ class App(customtkinter.CTk):
             
         self.search.bind('<Return>', moviesearched)
 
+        # Loads the WatchedMoviesFrame and sets its size and other variables
+        self.watchedmoviesframe = WatchedMoviesFrame(self, width=1100, height=500, corner_radius=20)
+        self.watchedmoviesframe.grid(pady=10,padx=10,row=1,column=0,columnspan=16,sticky="nsew")
+            
+
 # Frame that has all the watched movies
-class WatchedMoviesFrame(customtkinter.CTkFrame):
+class WatchedMoviesFrame(customtkinter.CTkScrollableFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.label = customtkinter.CTkLabel(self)
-        self.label.grid(row=0, column=0, padx=20)
 
+        # This loops through all movies and then provides a spot for the empty + movie cover.
+        for movie_index in range(0, len(watchedmovies) + 1):
+            movie_row = (1, 0)[movie_index % 2 == 0] # Basic tenerary operator meaning that ensures the ordering of the movies in which rows.
+            if movie_row == 0:
+                movie_column = math.floor((movie_index) / 2)
+            else:
+                movie_column = math.floor((movie_index) + 1 / 2) # Some tenerary operators in the following line, basically just validates the movie
+            movie_frame = MovieFrame(self, watchedmovies[movie_index] if not movie_index >= len(watchedmovies) else None, False if not movie_index >= len(watchedmovies) else True, width=400, height=600)
+            movie_frame.grid(row=movie_row,column=movie_column,padx=10,pady=10)
+
+class MovieFrame(customtkinter.CTkFrame):
+    def __init__(self, master, StoredMovie, empty, **kwargs):
+        super().__init__(master, **kwargs)
+
+        if empty:
+            self.image = Images.open(os.path.dirname(os.path.realpath(__file__)) + r"\\storage\\empty.png")
+        else:
+            self.image = Images.open(requests.get(self.StoredMovie.coverstoragelocation, stream=True).raw)
+        self.cover = customtkinter.CTkImage(light_image=self.image,size=(400, 600))
+
+        self.label = customtkinter.CTkLabel(self, image=self.cover)
+        self.label.pack()
         
+
+""" This is the main code sections where we will connect the GUI into the modules and storage
+    First we have a class for the movies to be stored
+    and then the rest of the code
+"""
+# Movie Storage Class
+class StoredMovie():
+    def __init__(self):
+        self.title = "" # Sets up the values within the class
+        self.TMDbID = 0
+        self.coverstoragelocation = ""
         
+    def setvalues(title, TMDbID, coverstoragelocation): # Validates all the values to ensure they aren't wrongly typed.
+        if isinstance(title, (str, unicode)):
+            self.title = title
+        else:
+            print("Title is not a string... converting")
+            self.title = str(title)
+        if isinstance(TMdbID, int):
+            self.TMDbID = TMDbID
+        else:
+            print("TMDbID is not a number... attempting conversion")
+            try:
+                self.TMDbID = int(TMDbID)
+            except:
+                print("Could not convert TMDbID to a number! Returning")
+                return
+        if isinstance(coverstoragelocation, (str, unicode)):
+            self.coverstoragelocation = ""
+        else:
+            print("Cover Storage Location is not a string... converting")
+            self.coverstoragelocation = str(coverstoragelocation)
+
+watchedmovies = [] # Temporary, this should be read from a file.
+
+""" Back to running the GUI """        
 # Runs the App
 app = App()
 app.columnconfigure(0, weight=1)
 app.mainloop()
+
